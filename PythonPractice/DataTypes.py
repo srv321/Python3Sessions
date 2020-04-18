dfc5a1bdcfe1c98e3bdd0edd4db18be9fdde2666
Saurav Saha <srv321@gmail.com>
2020-04-18 22:03:19 +0530

    updated

--- PythonPractice/DataTypes.py
+++ PythonPractice/DataTypes.py
@@ -1,16 +1,16 @@
-#no need to declare any type of variable
+# no need to declare any type of variable
 
-i=32
+i = 32
 print(i)
 
-i=32+0.1
+i = 32 + 0.1
 print(i)
 
-i="test"
+i = "test"
 print(i)
 
-x=50
-y=x
+x = 50
+y = x
 
 print(x)
 print(y)
@@ -18,48 +18,29 @@ print(y)
 print(id(x))
 print(id(y))
 
-#A-Z a-z 0-9
+# A-Z a-z 0-9
 min_bal = 100
 print(min_bal)
 
-number_of_months =12
+number_of_months = 12
 numberOfMonths = 12
-print(number_of_months,numberOfMonths)
+print(number_of_months, numberOfMonths)
 
 miles = 100.0
-name= "Tom"
-print(miles,name)
+name = "Tom"
+print(miles, name)
 
-a=b=c=1
-print(a,b,c)
+a = b = c = 1
+print(a, b, c)
 
-a,b,c= 3,4,"Saurav"
-print(a,b,c)
+a, b, c = 3, 4, "Saurav"
+print(a, b, c)
 
-s1="Hello World"
+s1 = "Hello World"
 
 print(s1)
 print(s1[0])
 print(s1[2:6])
 print(s1[2:])
-print(s1*3)
-print(s1+" "+ "python")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(s1 * 3)
+print(s1 + " " + "python")
