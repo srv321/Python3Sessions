bd266ac99110fd61f5312fe051ed909ef40f5f13
Saurav Saha <srv321@gmail.com>
2020-04-18 22:00:28 +0530

    Adding first python code

---
+++ PythonPractice/DataTypes.py
@@ -0,0 +1,65 @@
+#no need to declare any type of variable
+
+i=32
+print(i)
+
+i=32+0.1
+print(i)
+
+i="test"
+print(i)
+
+x=50
+y=x
+
+print(x)
+print(y)
+
+print(id(x))
+print(id(y))
+
+#A-Z a-z 0-9
+min_bal = 100
+print(min_bal)
+
+number_of_months =12
+numberOfMonths = 12
+print(number_of_months,numberOfMonths)
+
+miles = 100.0
+name= "Tom"
+print(miles,name)
+
+a=b=c=1
+print(a,b,c)
+
+a,b,c= 3,4,"Saurav"
+print(a,b,c)
+
+s1="Hello World"
+
+print(s1)
+print(s1[0])
+print(s1[2:6])
+print(s1[2:])
+print(s1*3)
+print(s1+" "+ "python")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
